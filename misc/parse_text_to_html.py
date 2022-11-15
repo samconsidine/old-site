6e78da06cc36263a94bf50d8b4476af39f556865
Sam Considine <sambconsidine@gmail.com>
2022-11-15 10:23:29 +0000

    Story test

---
+++ misc/parse_text_to_html.py
@@ -0,0 +1,36 @@
+import sys
+
+
+preamble = """<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sam's site</title>
+</head>
+<body>
+
+<style>
+    p {margin-bottom: 0; margin-top: 0; font-family: Georgia, 'Times New Roman', Times, serif;}
+</style>
+"""
+
+print(preamble)
+
+with open(sys.argv[1]) as f:
+    lines = f.readlines()
+    
+    for line in lines:
+        if line.strip() == '':
+            print('<br>')
+        else:
+            formatted_line = line.strip()
+            formatted_line = formatted_line.replace("'", '&#39')
+            print('<p>' + formatted_line.replace('\n', '') + '</p>')
+
+postamble = """</body>
+</html>
+"""
+
+print(postamble)
